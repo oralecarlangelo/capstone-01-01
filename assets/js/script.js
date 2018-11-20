2a1b046507f7a092d886cd05aac0053c5256704b
Carl Angelo Orale <oralecarlangelo@gmail.com>
2018-11-20 15:27:49 +0800

    first push capstone 1

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+  var hash = window.location.hash;
+  hash && $('ul.nav.nav-pills a[href="' + hash + '"]').tab('show'); 
+  $('ul.nav.nav-pills a').click(function (e) {
+     $(this).tab('show');
+     var scrollmem = $('body').scrollTop();
+     window.location.hash = this.hash;
+  });
+});
